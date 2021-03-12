f401b5222028ad503b32770c4f5310cbe2de4ac4
JeeJee <jjkwanhere@gmail.com>
2021-03-12 17:55:59 +0800

    Creating a User Interface

--- code.js
+++ code.js
@@ -31,6 +31,21 @@ figma.ui.onmessage = (msg) => {
                 node.opacity *= 0.5;
             }
         }
+    } else if (msg.type === "recursive") {
+        let count = 0;
+        function traverse(node) {
+            if ("children" in node) {
+                count++;
+                if (node.type !== "INSTANCE") {
+                    for (const child of node.children) {
+                        traverse(child);
+                    }
+                }
+            }
+        }
+        traverse(figma.root);
+        // 向 UI 发送信息
+        figma.ui.postMessage(count);
     } else if (msg.type === "cancel") {
         figma.closePlugin();
     }
